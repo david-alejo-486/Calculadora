c792aee6d643535d68ad3ddeeea4a2a3b8914fb6
david-alejo-486 <dabotacheto58@inemkennedy.edu.co>
2026-06-02 22:47:48 -0500

    Calculadora con puentes y limpia

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-var suma = function (){
-    var valorx = document.getElementById('valor1').value
-    var valory = document.getElementById('valor2').value
-    document.getElementById('resultado').innerHTML= "El resultado de la suma es: " + (parseInt (valorx)+ parseInt(valory))
+function suma() {
+    var valorx = document.getElementById('valor1').value;
+    var valory = document.getElementById('valor2').value;
+    document.getElementById('resultado').innerHTML = "El resultado de la suma es: " + (parseInt(valorx) + parseInt(valory));
 }
 
-var resta = function (){
-    var valorx = document.getElementById('valor1').value
-    var valory = document.getElementById('valor2').value
-    document.getElementById('resultado').innerHTML= "El resultado de la resta es: " + (parseInt (valorx) - parseInt(valory))
+function resta() {
+    var valorx = document.getElementById('valor1').value;
+    var valory = document.getElementById('valor2').value;
+    document.getElementById('resultado').innerHTML = "El resultado de la resta es: " + (parseInt(valorx) - parseInt(valory));
 }
 
-var multiplicacion = function (){
-    var valorx = document.getElementById('valor1').value
-    var valory = document.getElementById('valor2').value
-    document.getElementById('resultado').innerHTML= "El resultado de la multiplicacion es: " + (parseInt (valorx) * parseInt(valory))
+function multiplicacion() {
+    var valorx = document.getElementById('valor1').value;
+    var valory = document.getElementById('valor2').value;
+    document.getElementById('resultado').innerHTML = "El resultado de la multiplicacion es: " + (parseInt(valorx) * parseInt(valory));
 }
 
-var division = function (){
-    var valorx = document.getElementById('valor1').value
-    var valory = document.getElementById('valor2').value
-    document.getElementById('resultado').innerHTML= "El resultado de la division es: " + (parseInt (valorx) / parseInt(valory))
+function division() {
+    var valorx = document.getElementById('valor1').value;
+    var valory = document.getElementById('valor2').value;
+    
+    if (parseInt(valory) === 0) {
+        document.getElementById('resultado').innerHTML = "No se puede dividir por cero";
+    } else {
+        document.getElementById('resultado').innerHTML = "El resultado de la division es: " + (parseInt(valorx) / parseInt(valory));
+    }
 }
